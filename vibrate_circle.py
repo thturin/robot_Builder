0fea88d45d0f5509d6b6674c981ac00718bc4631
tatiana <tatiana.turin@gmail.com>
2022-06-30 14:43:44 -0400

    commit

--- vibrate_circle.py
+++ vibrate_circle.py
@@ -1,23 +1,20 @@
 import turtle as t
+import random
 
-s = t.Screen()
 def buttonclick(x,y):
     print("You clicked at this coordinate({0},{1})".format(x,y))
 
-
+s = t.Screen()
+s.colormode(255)
 s.setup(width=0.5,height=0.5) #half of the screens dimensions
 t.onscreenclick(buttonclick,1)
 t.listen()
+print('The canvas size is {0} x {1} '.format(s.window_width(),s.window_height()))
 
-screen = t.getscreen()
-w = screen.window_width()
-l = screen.window_height()
-print( l,w)
 t.speed('fast')
 t.bgcolor('black')
 t.pencolor('white')
-#t.penup()
-t.goto(w/2,l/2)
+t.goto(0,100)
 #t.pendown()
 a=0
 b=0
@@ -26,5 +23,7 @@ while True:
     t.right(b) # angle
     a+=2
     b+=1
+    t.pencolor((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
     if b==210:
         break
+
